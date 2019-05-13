43ac0462800684b475e0e15c2be70c9a9501960f
BNPrashanth <bnprashanth256@gmail.com>
2019-05-13 10:55:04 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'authors-api/v1'], function ($router) {
+    $router->get('/all', 'AuthorController@index');
+    $router->post('/add', 'AuthorController@store');
+    $router->get('/get/{author}', 'AuthorController@show');
+    $router->put('/update/{author}', 'AuthorController@update');
+    $router->patch('/update/{author}', 'AuthorController@update');
+    $router->delete('/delete/{author}', 'AuthorController@destroy');
+});
+
+$router->group(['prefix' => 'books-api/v1'], function ($router) {
+    $router->get('/all', 'BookController@index');
+    $router->post('/add', 'BookController@store');
+    $router->get('/get/{book}', 'BookController@show');
+    $router->put('/update/{book}', 'BookController@update');
+    $router->patch('/update/{book}', 'BookController@update');
+    $router->delete('/delete/{book}', 'BookController@destroy');
 });
