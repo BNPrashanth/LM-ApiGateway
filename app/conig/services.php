5687cb9191237fb3c850f638d56d19210c0339a9
BNPrashanth <bnprashanth256@gmail.com>
2019-05-13 11:13:59 +0530

    services config added and registered, env.emaple updated with base_uri for microservices

---
+++ app/conig/services.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'authors' => [
+        'basr_uri' => env('AUTHORS_SERVICE_BASE_URL')
+    ],
+
+    'books' => [
+        'basr_uri' => env('BOOKS_SERVICE_BASE_URL')
+    ],
+
+];
+
+?>
