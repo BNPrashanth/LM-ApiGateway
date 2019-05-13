4713fc5d37fe48b85a06ae8ea0a1e586b4d2c58d
BNPrashanth <bnprashanth256@gmail.com>
2019-05-13 12:52:53 +0530

    remaining methods related to books service and controller completed

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -12,11 +12,73 @@ class BookService
      * The base uri to consume the books service
      * @var string
      */
-    public $base_uri;
+    public $baseUri;
 
     public function __construct()
     {
-        $this->base_uri = config('services.books.base_uri');
+        $this->baseUri = config('services.books.base_uri');
+    }
+
+    /**
+     * Obtain full list of Books from the BooksMicroservice
+     * @return string
+     */
+    public function obtainBooks()
+    {
+        return $this->performRequest(
+            'GET',
+            'books-api/v1/all'
+        );
+    }
+
+    /**
+     * Create a new Book from the BooksMicroservice
+     * @return string
+     */
+    public function createBook($data)
+    {
+        return $this->performRequest(
+            'POST',
+            'books-api/v1/add',
+            $data
+        );
+    }
+
+    /**
+     * Obtain an existing instance of an Book from the BooksMicroservice
+     * @return string
+     */
+    public function obtainBook($book)
+    {
+        return $this->performRequest(
+            'GET',
+            "books-api/v1/get/{$book}"
+        );
+    }
+
+    /**
+     * Update an existing instance of an Book from the BooksMicroservice
+     * @return string
+     */
+    public function editBook($data, $book)
+    {
+        return $this->performRequest(
+            'PUT',
+            "books-api/v1/update/{$book}",
+            $data
+        );
+    }
+
+    /**
+     * Remove an existing instance of an Book from the BooksMicroservice
+     * @return string
+     */
+    public function deleteBook($book)
+    {
+        return $this->performRequest(
+            'DELETE',
+            "books-api/v1/delete/{$book}"
+        );
     }
 
 }
